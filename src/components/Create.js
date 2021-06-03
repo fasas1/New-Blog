374888764aa474f1a59b3e76a79b2833f90c0b26
fasasi qasim <fasaqa1000@gmail.com>
2021-06-03 07:11:28 -0700

    added a form input

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,7 +1,14 @@
 const Create = () => {
     return ( 
          <div>
-             <h2>New Blog</h2>
+             <h2>Add Blog</h2>
+          <form>
+              <label>Title</label>
+               <input type="text" />
+               <label>Body</label>
+               <input type="text" />
+          </form>
+
          </div>
      );
 }
